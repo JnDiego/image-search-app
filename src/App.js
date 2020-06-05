ce6b102af706374041a77670864e45300f676ac9
Juan Diego <diego_suarez@outlook.com>
2020-06-04 19:45:17 -0500

    Se crea componente Form, encargado de recibir la palabra de busqueda en la API de imagenes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
-  return <h1>Works</h1>;
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Image Search</p>
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form action="">
+      <div className="row">
+        <div className="form-group col-md-8">
+          <input type="text" className="form-control form-control-lg" placeholder="Search a image..." />
+        </div>
+        <div className="form-group col-md-4">
+          <input type="submit" className="btn btn-lg btn-danger btn-block" value="Search" />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
