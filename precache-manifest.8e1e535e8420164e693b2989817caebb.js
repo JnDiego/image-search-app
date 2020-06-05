fb57dabb938e25b926d15d69dfd80d88ff1595b2
Juan Diego <diego_suarez@outlook.com>
2020-06-04 21:01:01 -0500

    Updates

---
+++ precache-manifest.8e1e535e8420164e693b2989817caebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cd0b040fa0b61d99c3647fecc02e6c",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "7c258daf570bee6443ff",
+    "url": "/image-search-app/static/js/2.1694ad23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search-app/static/js/2.1694ad23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae88b15650c439808e96",
+    "url": "/image-search-app/static/js/main.5e5b2552.chunk.js"
+  },
+  {
+    "revision": "3912c15f0f47b175934e",
+    "url": "/image-search-app/static/js/runtime-main.fb4fb76d.js"
+  }
+]);
